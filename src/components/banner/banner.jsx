52ba8fae1b06be220c7e2802dfc42f4f3410dc62
wfhgdev <willifhernandez@gmail.com>
2026-08-27 18:36:49 +0200

    feature: CSS Style added to try view

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './Banner.css';
 
 const apiUrl = 'https://api.escuelajs.co/api/v1/products';
 const maxBannerItems = 5;
