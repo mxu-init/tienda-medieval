6c82b9814d38949ff7e953fc4418522d5560527e
mxu-init <14923949+mxu-init@users.noreply.github.com>
2026-08-27 13:54:06 +0200

    feat: initialize project structure with React Router and main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,25 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+// import Header from "./components/Header.jsx";
+// import Footer from "./components/Footer.jsx";
+
+function App() {
+  return (
+    <div className="">
+      {/* <Header /> */}
+      <main className="">
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          {/* <Route path="/pisos" element={<Apartments />} />
+          <Route path="/historia" element={<History />} />
+          <Route path="/agentes" element={<Agents />} />
+          <Route path="/contacto" element={<ContactSection />} />
+          <Route path="/menu" element={<Menu />} /> */}
+        </Routes>
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
