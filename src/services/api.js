27b09a6160b1477148b0811b85e052832fb215ac
wfhgdev <willifhernandez@gmail.com>
2026-09-01 13:51:00 +0200

    feature: JSON Server alternative added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.escuelajs.co/api/v1',
+  baseURL: 'http://localhost:5000',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
