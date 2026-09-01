14dc7d56bd47cb8258e11f1aab531046655f1321
mxu-init <14923949+mxu-init@users.noreply.github.com>
2026-09-01 09:53:57 +0200

    fix: lowe case Header folder name problem

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
-import Header from "./components/Header/Header";
+import Header from "./components/Headerx/Header";
 import Footer from "./components/Footer/Footer";
 
 function App() {
